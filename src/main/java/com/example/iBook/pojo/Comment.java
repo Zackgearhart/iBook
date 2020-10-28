8d0d304aa35fba6fe58d1693b3fefb1c26f8ee01
Zack <zackgearhart65@gmail.com>
2020-10-27 20:54:25 -0500

    Initial commit

---
+++ src/main/java/com/example/iBook/pojo/Comment.java
@@ -0,0 +1,75 @@
+package com.example.iBook.pojo;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="comment_id")		
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="comment_user_id", nullable = false)
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name="comment_post_id", nullable = false)
+	private Post post;
+	
+	@Column(name="comment_text")
+	private String content;
+	
+	@Column(name="comment_date")
+	private Date date;
+
+	public int getId() {
+		return id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
